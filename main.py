42fd1fa85f4fcc7ca36ff7fe8f3e817a07bb325d
vcokltfre <vcokltfre@gmail.com>
2021-01-26 23:31:43 +0000

    feat: add roles and response

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.exceptions import HTTPException
-from discord_interactions import verify_key, InteractionResponseType, InteractionType
+from discord_interactions import verify_key, InteractionResponseType,
 
-from json import dumps
-
-from src.models import Interaction
-from config import PUBKEY
+from config import PUBKEY, roles
 
 app = FastAPI(docs_url=None)
 
@@ -23,11 +20,32 @@ async def interactions(req: Request):
     if data["type"] == 1:
         return {"type":1}
 
-    if data["type"] == InteractionType.APPLICATION_COMMAND:
-        print(dumps(data))
+    rls = set(data["member"]["roles"]) & set(roles)
+
+    if not rls:
         return {
-            "type": InteractionResponseType.CHANNEL_MESSAGE,
-            "data": {
-                "content": "Test success!"
-            }
+        "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        "data": {
+            "content": "You do not have permissions to use this command!"
         }
+    }
+
+    ops = data["data"]["options"]
+    action = ops[0]["value"]
+    server = ops[1]["value"]
+    mcname = ops[2]["value"]
+    pgroup = ops[3]["value"] if len(ops) == 4 else None
+
+    if action == "op_grant":
+        content = f"Granted permission group {pgroup} on {server} to {mcname}"
+    elif action == "op_revoke":
+        content = f"Revoked permission group {pgroup} on {server} from {mcname}"
+    else:
+        content = "Not implemented"
+
+    return {
+        "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        "data": {
+            "content": content
+        }
+    }
